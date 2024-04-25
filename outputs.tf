8ee55a90df6132187e117aad4186cb2f7fbccd3d
Michael Ullrich <michael.ullrich@nuvibit.com>
2024-04-25 13:15:28 +0200

    fix

--- outputs.tf
+++ outputs.tf
@@ -7,11 +7,20 @@ output "ou_paths_with_id" {
 }
 
 output "scp_policies_details" {
-  value =  {
+  value = {
     for key, policy in aws_organizations_policy.scp_policies : key => {
       name          = policy.name
+      arn           = policy.arn
       scp_id        = policy.id
       policy_length = length(policy.content)
     }
   }
+}
+
+output "aws_organizations_policy_ou_attachment" {
+  value = aws_organizations_policy_attachment.ou_attachment
+}
+
+output "aws_organizations_policy_account_attachment" {
+  value = aws_organizations_policy_attachment.account_attachment
 }
